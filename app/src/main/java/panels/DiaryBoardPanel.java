22233a072db92191476feab1aa7e6b379272e6b3
myunghoonkim <xcv8826@gmail.com>
2022-07-18 11:40:24 +0900

    20220718-myDiary-kmh
    삭제기능 디스플레이에 안나타나던 안되던것 복구

--- app/src/main/java/panels/DiaryBoardPanel.java
+++ app/src/main/java/panels/DiaryBoardPanel.java
@@ -15,7 +15,7 @@ public class DiaryBoardPanel extends JPanel {
   private JFrame writingFrame;
   private JTextField titleTextField;
   private JTextArea writingTextArea;
-  private Writing writing;
+
 
   private List<Writing> writings = new ArrayList<>();
 
@@ -60,7 +60,7 @@ public class DiaryBoardPanel extends JPanel {
           String  title = titleTextField.getText();
           String  writingContent = writingTextArea.getText();
             //라이팅 생성
-            writing = new Writing(title,writingContent,"ORIGINAL");
+            Writing writing = new Writing(title,writingContent,"ORIGINAL");
             writings.add(writing);
 
             writingContentButton = new WritingContentButton(writing);
@@ -100,12 +100,13 @@ public class DiaryBoardPanel extends JPanel {
                 writing.deleted();
                 writingFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
                 writingFrame.setVisible(false);
+                writingListPanel.removeAll();
                 writingListPanel = new WritingListPanel(writings,writingContentButton);
                 //패널 뉴 -> 버튼들 (패널1)-> 지운다 ,상태바꾼다 -> 셋비지블 보여라 (패널1)->
                 refresh();
               });
               //여기부터 새로만든거
-              modifyButton.addActionListener(event2 -> {
+             /* modifyButton.addActionListener(event2 -> {
                     deleteModifyPanel.removeAll();
                     deleteModifyPanel.setLayout(new GridLayout(1, 3));
                     JButton modifySaveButton = new JButton("저장하기");
@@ -122,7 +123,9 @@ public class DiaryBoardPanel extends JPanel {
                      String modifiedWritingContent = writingTextArea.getText();
                      titleTextField.setText(modifiedtTitle);
                      writingTextArea.setText(modifiedWritingContent);
-                     writing = new Writing(modifiedtTitle,modifiedWritingContent,"MODIFIED");
+                      titleTextField.setEditable(false);
+                      writingTextArea.setEditable(false);
+
                      writingFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
                      writingFrame.setVisible(false);
                      writingListPanel.removeAll();
@@ -131,7 +134,7 @@ public class DiaryBoardPanel extends JPanel {
                      refresh();
                     });
                   });
-
+*/
               //여기까지
             });
           }
@@ -140,9 +143,13 @@ public class DiaryBoardPanel extends JPanel {
     });
   }
   public void refresh() {
+
     this.add(writingListPanel);
+    this.setVisible(false);
+    this.setVisible(true);
     writingListPanel.setVisible((false));
     writingListPanel.setVisible(true);
+
   }
   private void refreshDisplay() {
     writingFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
